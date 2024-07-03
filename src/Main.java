26ee736d070a094ab1e611c839e8ee8393e3fd84
SdneyFernandes <fsidney987@gmail.com>
2024-07-03 16:29:09 -0300

    Aplicando Reflections

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         TabelaProcessor.processTabela(MyClass.class);
+        Class<?> clazz = MyClass.class;
+        readTabelaAnnotation(clazz);
+    }
+
+    public static void readTabelaAnnotation(Class<?> clazz) {
+        if (clazz.isAnnotationPresent(Tabela.class)) {
+            Tabela tabelaAnnotation = clazz.getAnnotation(Tabela.class);
+            String tabelaName = tabelaAnnotation.value();
+            System.out.print("Tabela name: " + tabelaName);
+        }
     }
 }
